8e6b45ea9a8762376cdec15d0e2fcff9160c4f9b
Alexander Johnston <Aliics@hotmail.com>
2020-03-15 16:33:03 +1300

    Add environment option! implementation

--- src/lib.rs
+++ src/lib.rs
@@ -32,23 +32,49 @@ macro_rules! flag {
     };
 }
 
+/// Optional command line argument with associated value.
+///
+/// When provided via command line it will return `Some` wrapping
+/// the value passed along with it. `None` will be returned when
+/// the option is not provided or does not follow syntax.
+/// ```
+/// use ezcli::option;
+///
+/// option!(my_option);
+/// ```
+/// In some case of not wanting to use the program's environment
+/// arguments using a slice is also possible.
+/// ```
+/// use ezcli::option;
+///
+/// let args = ["--my_option", "value"];
+/// flag!(my_option, args);
+/// ```
 #[macro_export]
-macro_rules! optional_arg {
+macro_rules! option {
     ($name:tt, $args:ident) => {
-        let $name: Option<&str> = {
-            let mut i = 0;
-            let mut optional = None;
-            let wanted_arg = format!("--{}", stringify!(my_arg));
-            while (i < $args.len()) {
-                if $args[i] == wanted_arg && $args.len() > i + 1 {
-                    optional = Some($args[i + 1]);
-                    break;
-                }
-
-                i += 1;
-            }
-
-            optional
+        let $name: Option<String> = {
+            let args: Vec<String> = $args.iter().map(|s| s.to_string()).collect();
+            $crate::_option(stringify!($name), args.as_slice())
         };
     };
+    ($name:tt) => {
+        let $name: Option<String> = {
+            let args: Vec<String> = std::env::args().collect();
+            $crate::_option(stringify!($name), args.as_slice())
+        };
+    };
+}
+
+pub fn _option(name: &str, args: &[String]) -> Option<String> {
+    let mut optional = None;
+    let wanted_arg = format!("--{}", name);
+    for i in 0..args.len() {
+        if args[i] == wanted_arg && args.len() > i + 1 {
+            optional = Some(args[i + 1].clone());
+            break;
+        }
+    }
+
+    optional
 }
--- tests/optional_arg.rs
+++ tests/optional_arg.rs
@@ -1,10 +1,28 @@
-use ezcli::optional_arg;
+use ezcli::option;
 
 #[test]
 fn should_return_some_when_argument_provided() {
     let args = ["--my_arg", "value"];
 
-    optional_arg!(my_arg, args);
+    option!(my_arg, args);
 
     assert_eq!("value", my_arg.unwrap());
+}
+
+#[test]
+fn should_return_none_when_no_arguments_provided() {
+    option!(will_be_none);
+
+    assert!(will_be_none.is_none());
+}
+
+#[test]
+fn should_return_two_somes_when_given_two_arguments() {
+    let args = ["--my_arg0", "big", "--my_arg1", "wuv"];
+
+    option!(my_arg0, args);
+    option!(my_arg1, args);
+
+    assert_eq!("big", my_arg0.unwrap());
+    assert_eq!("wuv", my_arg1.unwrap());
 }
